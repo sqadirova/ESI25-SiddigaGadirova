4807acea30a03e3027ce297fc619ba5c224f715e
Siddiga Gadirova <sigadi@taltech.ee>
2025-06-09 15:14:15 +0300

    Task 1 done

--- studentservice/src/main/java/com/esi/studentservice/controller/StudentController.java
+++ studentservice/src/main/java/com/esi/studentservice/controller/StudentController.java
@@ -2,15 +2,14 @@ package com.esi.studentservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.esi.studentservice.dto.LeaveRequestDto;
 import com.esi.studentservice.service.StudentService;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,4 +25,11 @@ public class StudentController {
         return ResponseEntity.ok("A leave request has been created");
 
     }
+
+//    Task 1
+    @GetMapping("/leaverequests")
+    public List<LeaveRequestDto> getAllLeaveRequests() {
+        return studentService.getAllleaveRequests();
+    }
+
 }
